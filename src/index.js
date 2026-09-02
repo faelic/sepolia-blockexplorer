3b4d35162f9083927950b1f41b11dbeeb22d83c8
Favour Ututu <ututufavour6@gmail.com>
2026-09-02 18:33:25 +0100

    typography polish

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import '@fontsource-variable/manrope';
-import '@fontsource/ibm-plex-mono';
+import '@fontsource/ibm-plex-mono/400';
+import '@fontsource/ibm-plex-mono/500';
+import '@fontsource/ibm-plex-mono/600';
 import './index.css';
 import App from './App';
 
